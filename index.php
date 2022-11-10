a57b10536a80eb4c6d6ca14af8bc6988327f7cf2
AulerLucas <lucasaulerbonfante@yahoo.com.br>
2022-11-10 20:11:45 -0300

    :zap:

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title> exercicio1 </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+ 
+    </head>
+    <body>
+        <form action="exercicio4.php" method="POST">
+            <h1 id="til"> Comversor de data em padrão americano</h1>
+        
+            <p id="d">
+            Informe a data no devido formado:  <input type="text" name="d"  placeholder="2022-11-10" />
+            </p>
+
+<input type="submit" value="Converter agora" name='con' id="con">
+        </form>
+    </body>
+</html>
